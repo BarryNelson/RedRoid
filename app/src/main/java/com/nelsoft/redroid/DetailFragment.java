50dc7285d93818a1e86d19f9db70b021b1c4a886
Barry Nelson <barry100001@gmail.com>
2015-11-16 20:21:40 -0800

    Selected item now shows in WebView

--- app/src/main/java/com/nelsoft/redroid/DetailFragment.java
+++ app/src/main/java/com/nelsoft/redroid/DetailFragment.java
@@ -8,10 +8,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 
 
 /**
@@ -35,7 +35,7 @@ public class DetailFragment extends Fragment {
     private String title;
 
     private OnFragmentInteractionListener mListener;
-    private ImageView detailImage;
+    private WebView detailImage;
     private TextView detailTitle;
     private TextView detailAuthor;
     private Context context;
@@ -77,16 +77,20 @@ public class DetailFragment extends Fragment {
     private void display() {
         detailTitle.setText(title);
         detailAuthor.setText(author);
-        Glide.with(context)
-                .load(strURL)
-                .into(detailImage);
+//        Glide.with(context).load(strURL).into(detailImage);
+        detailImage.getSettings().setJavaScriptEnabled(true);
+        detailImage.setWebViewClient(new WebViewClient());
+        detailImage.getSettings().setDomStorageEnabled(true); // localStorage
+        detailImage.setWebChromeClient(new WebChromeClient()); // for alerts to appear
+        detailImage.loadUrl(strURL);
     }
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        detailImage = (ImageView) view.findViewById(R.id.detailImage);
+//        detailImage = (ImageView) view.findViewById(R.id.detailImage);
+        detailImage = (WebView) view.findViewById(R.id.detailImage);
         detailTitle = (TextView) view.findViewById(R.id.detailTitle);
         detailAuthor = (TextView) view.findViewById(R.id.detailAuthor);
         return view;
